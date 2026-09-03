f630b7ebb51b750afec3f6d4c80d591763d7f98d
Shaya Teller <shaya.teller@gmail.com>
2026-09-04 00:44:40 +0300

    kkk

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -24,7 +24,7 @@ export default function Homepage() {
       <div className="container topbar-inner">
         <a href="#home" className="brand" aria-label="POSSIBLE home page">
           <div className="brand-lockup">
-            <img src="public\myLogo.png" alt="" className="logo" />
+            <img src="/myLogo.png" alt="" className="logo" />
           </div>
         </a>
       </div>
